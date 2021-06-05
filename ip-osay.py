7e3593fcd9ad003aae3a508191dc555e6125d527
Osay2 <84993769+Osay2@users.noreply.github.com>
2021-06-05 03:20:46 -0300

    Add files via upload

---
+++ ip-osay.py
@@ -0,0 +1,56 @@
+import urllib.request
+import json
+import os
+
+blue = "\033[1;34;40m"
+reset = "\033[0;37;40m"
+cyan = "\033[1;36;40m"
+red = "\033[1;31;40m"
+green = "\033[1;32;40m"
+yellow = "\033[1;33;40m"
+
+
+os.system("clear")
+print()
+print()
+
+print("""
+╔══╦═══╗─╔═══╗
+╚╣╠╣╔═╗║─║╔═╗║
+─║║║╚═╝║─║║─║╠══╦══╦╗─╔╗
+─║║║╔═╦╩═╣║─║║══╣╔╗║║─║║
+╔╣╠╣║─╚══╣╚═╝╠══║╔╗║╚═╝║
+╚══╩╝────╚═══╩══╩╝╚╩═╗╔╝
+───────────────────╔═╝║
+───────────────────╚══╝""")
+print(red+"""					by Osay""")
+print()
+print()
+
+print()
+peticion = input(yellow+"Ingrese la IP de la victima: ")
+print()
+
+print(red+"Datos:\n")
+print(reset+" ")
+
+def main():
+	ip_del_vato = "https://ipinfo.io/"+peticion+"/json"
+	url = urllib.request.urlopen(ip_del_vato)
+	
+	nya = json.loads(url.read())
+	
+	
+	for dato in nya:
+		
+		print(dato+":"+nya[dato])
+	
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		quit()
+		
+		
+		
+print(reset+" ")
